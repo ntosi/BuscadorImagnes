e628fbb7ae9ab663cdc165f2f6daa59dbe10be97
Nico <nicolas.tosi@gmail.com>
2020-02-09 13:05:18 -0300

    estaba mal el link al css

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -5,7 +5,9 @@ class SearchBar extends React.Component{
     render (){
         return (<div className="ui segment">
             <form className="ui form">
-                <div></div>
+                <div className="field">
+                    <label> Busqueda de personas</label>
+                </div>
                 <input type="text" />
             </form>
         </div>
