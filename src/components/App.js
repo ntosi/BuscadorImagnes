6b3df70204df94d301f8e6f48d7b5ca2bebaf794
Nico <nicolas.tosi@gmail.com>
2020-02-09 14:13:16 -0300

    .

--- src/components/App.js
+++ src/components/App.js
@@ -5,9 +5,11 @@ import SearchBar from './SearchBar'
 const App = () =>{
   return (
     <div className="ui container" style={{ marginTop: '10px'}}>
-      <SearchBar />
-      hola
+      
       la hora es:<Clock />
+      <SearchBar />
+      
+      
     </div>
   )
 }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,14 +2,22 @@ import React from 'react'
 
 
 class SearchBar extends React.Component{
+    state = { term: ''}
+
+    
     render (){
         return (<div className="ui segment">
             <form className="ui form">
                 <div className="field">
                     <label> Busqueda de personas</label>
                 </div>
-                <input type="text" />
+                <input 
+                type="text"
+                value={ this.state.term }
+                onChange={(e) => {this.setState({term: e.target.value})}}
+                />
             </form>
+                <div className="texto">{ this.state.term }</div>
         </div>
         )
     }
