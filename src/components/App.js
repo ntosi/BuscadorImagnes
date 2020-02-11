c1952220a1ab42aba9190a308472103d4a88dd8c
Nico <nicolas.tosi@gmail.com>
2020-02-11 13:28:39 -0300

    agregue axios y hago get a url de unsplash

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import axios from 'axios'
 import Clock from './Clock';
 import SearchBar from './SearchBar'
 
 class App extends React.Component{
   
   onSearchSubmit(term){
-    console.log(term)
+     
+    axios.get('https://api.unsplash.com/search/photos',{
+      params:{ query: term } ,
+      headers:{
+        Authorization: 'Client-ID 4c8a595e589ad737a363dfb7e6fc4e64ea9ccdf21df8eab6bb4c0a086c1ca42c'
+      }
+    })
+
 
   }
   
